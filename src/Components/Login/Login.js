961933bd6659958fc0132cfb64232c1fc482945f
“Lauren <lucerolaurenm@gmail.com>
2020-06-08 11:55:47 -0400

    Login and Link functionality

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,34 +1,52 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const Login = () => {
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Login = (props) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [purpose, setPurpose] = useState("");
+  const [error, setError] = useState("");
 
-  const handleClick = () => {};
+  const checkInputs = () => {
+    return username !== "" && password !== "" && purpose !== "";
+  };
+
+  const handleClick = () => {
+    checkInputs()
+      ? props.setUser({
+          username: username,
+          purpose: purpose,
+        })
+      : setError("Please complete all inputs to login.");
+  };
 
   return (
-    <div>
+    <Wrapper>
       <h2>Welcome to Black Stories Matter!</h2>
       <h3>
         Our mission is to share books by Black authors for readers to find
-        mirrors or windows in.
+        mirrors & windows in.
       </h3>
-      <h4>What are mirrors and windows?</h4>
+      <h4>What are mirrors & windows?</h4>
       <p>
-        Mirrors reflect a reader's own life. Seeing yourself, your family and
-        your culture being valued in the world of a book provides a powerful
-        sense of belonging.
+        Mirrors reflect a reader's own life. Seeing yourself, your family & your
+        culture being valued in the world of a book provides a powerful sense of
+        belonging.
       </p>
       <p>
         Books that are windows offer views into other experiences. Windows teach
-        people to understand and appreciate differences.
+        people to understand & appreciate differences.
       </p>
       <p>
         Please support the Black authors you find here by buying their books,
-        requesting them at libraries, or donating copies to schools and
-        community centers!
+        requesting them at libraries, or donating copies to schools & community
+        centers!
       </p>
       <h4>Login to find stories!</h4>
       <form>
@@ -40,6 +58,7 @@ const Login = () => {
           type="text"
           onChange={(e) => setUsername(e.target.value)}
         />
+        <br />
         <label htmlFor="password">Enter your username: </label>
         <input
           id="password"
@@ -48,6 +67,7 @@ const Login = () => {
           type="text"
           onChange={(e) => setPassword(e.target.value)}
         />
+        <br />
         <label htmlFor="purpose">Are you looking for mirrors or windows?</label>
         <select id="purpose" onChange={(e) => setPurpose(e.target.value)}>
           <option value="''"> - Please share why you are here - </option>
@@ -55,9 +75,12 @@ const Login = () => {
           <option value="Windows">Windows</option>
           <option value="Both">Both</option>
         </select>
-        <button onClick={handleClick}>Login</button>
+        <p>{error}</p>
+        <Link to={checkInputs() && "/Books"}>
+          <button onClick={handleClick}>Login</button>
+        </Link>
       </form>
-    </div>
+    </Wrapper>
   );
 };
 
