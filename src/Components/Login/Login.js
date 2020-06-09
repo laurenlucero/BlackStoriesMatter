5677591e7e6995b27346ce0420255171a4ed0ce2
“Lauren <lucerolaurenm@gmail.com>
2020-06-09 12:05:22 -0400

    Edit message

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -80,22 +80,22 @@ const Login = (props) => {
       <h2>Welcome to BSM!</h2>
       <h3>
         Our mission is to share books by Black authors where readers can find
-        mirrors & windows.
+        mirrors and windows.
       </h3>
-      <h4>What are mirrors & windows?</h4>
+      <h4>What are mirrors and windows in books?</h4>
       <p>
-        Mirrors reflect a reader's own life. Seeing yourself, your family & your
-        culture being valued in the world of a book provides a powerful sense of
-        belonging.
+        Mirrors reflect a reader's own life. Seeing yourself, your family and
+        your culture being valued in the world of a book provides a powerful
+        sense of belonging.
       </p>
       <p>
         Books that are windows offer views into other experiences. Windows teach
-        people to understand & appreciate differences.
+        people to understand and appreciate differences.
       </p>
       <p>
         Please support the Black authors you find here by buying their books,
-        requesting them at libraries, or donating copies to schools & community
-        centers!
+        requesting them at libraries, or donating copies to schools and
+        community centers!
       </p>
       <h3>Login to find stories!</h3>
       <form>
