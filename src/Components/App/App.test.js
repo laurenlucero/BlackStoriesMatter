2665987d30dea73628e2a4536aa704a42f0a6ea5
“Lauren <lucerolaurenm@gmail.com>
2020-06-09 15:34:30 -0400

    Intergration tests

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,17 +1,8 @@
 import { MemoryRouter as Router } from "react-router-dom";
 import { cleanup, fireEvent, render, waitFor } from "@testing-library/react";
-import "@testing-library/jest-dom/";
+import "@testing-library/jest-dom/extend-expect";
 import App from "./App";
 import React from "react";
-
-test("renders App", () => {
-  const { getByText } = render(
-    <Router>
-      <App />
-    </Router>
-  );
-  expect(getByText("Black Stories Matter")).toBeInTheDocument();
-});
 
 describe("App", () => {
   afterEach(cleanup);
@@ -51,9 +42,30 @@ describe("App", () => {
       { target: { value: "windows" } }
     );
     fireEvent.click(getByText("Login"));
-    const userWelcome = await waitFor(() =>
-      getByText("Welcome, lauren_reads.")
+    const logoutBtn = await waitFor(() => getByText("Logout"));
+    expect(logoutBtn).toBeInTheDocument();
+  });
+
+  it("should render book previews for selected option", async () => {
+    const { getByText, getByLabelText, getByPlaceholderText } = render(
+      <Router>
+        <App />
+      </Router>
     );
-    expect(userWelcome).toBeInTheDocument();
+    fireEvent.change(getByPlaceholderText("username"), {
+      target: { value: "lauren_reads" },
+    });
+    fireEvent.change(getByPlaceholderText("password"), {
+      target: { value: "abc123" },
+    });
+    fireEvent.change(
+      getByLabelText("Are you looking for mirrors or windows?"),
+      { target: { value: "windows" } }
+    );
+    fireEvent.click(getByText("Login"));
+    const filter = await waitFor(() => getByLabelText("Filter books:"));
+    fireEvent.change(filter, { target: { value: "Angela Johnson" } });
+    const bookPreview = await waitFor(() => getByText("VIOLET'S MUSIC"));
+    expect(bookPreview).toBeInTheDocument();
   });
 });
